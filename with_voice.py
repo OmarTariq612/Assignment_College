76942b5595c702c4a9467889fb48f6a3628370c1
OmarTariq612 <az2339913@gmail.com>
2020-05-09 07:44:48 +0200

    first commit

---
+++ with_voice.py
@@ -0,0 +1,57 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('voice', engine.getProperty('voices')[2].id)
+engine.setProperty('rate', 150)
+
+
+def speech(text):
+    engine.say(text)
+    print(text)
+    engine.runAndWait()
+
+
+# Having the ticket type from the user
+ticketType = input('Enter your ticket type (1/2) : ')
+# Having the seat number from the user
+seatNumber = input('Enter your seat number : ')
+
+# Checking if the user has entered numeric values or not
+if ticketType.isnumeric() and seatNumber.isnumeric():
+    # changing the data type of the variables (from str to int)
+    ticketType = int(ticketType)
+    seatNumber = int(seatNumber)
+
+    if ticketType == 2:
+        # making sure that the user hasn't entered an invalid seat number
+        if seatNumber <= 64:
+            # calculating the remainder
+            remainder = seatNumber % 4
+
+            if remainder == 0 or remainder == 1:
+                speech(f"Your seat number is {seatNumber} , you are beside a window")
+            else:
+                speech(f"Your seat number is {seatNumber} , you are not beside a window")
+        else:
+            speech('Enter a valid seat number at maximum 64 for the second class')
+
+    elif ticketType == 1:  # else if
+        # making sure that the user hasn't entered an invalid seat number
+        if seatNumber <= 47:
+            # calculating the remainder
+            remainder = seatNumber % 3
+
+            if remainder == 0 or remainder == 1 or seatNumber == 47:
+                speech(f"Your seat number is {seatNumber} , you are beside a window")
+
+            else:
+                speech(f"Your seat number is {seatNumber} , you are not beside a window")
+
+        else:
+            speech('Enter a valid seat number at maximum 47 for the first class')
+
+    else:
+        speech('please enter a valid ticket type (1/2) only')
+
+else:
+    speech("Please enter valid values")
---
+++ without_voice.py
@@ -0,0 +1,43 @@
+# Having the ticket type from the user
+ticketType = input('Enter your ticket type (1/2) : ')
+# Having the seat number from the user
+seatNumber = input('Enter your seat number : ')
+
+# Checking if the user has entered numeric values or not
+if ticketType.isnumeric() and seatNumber.isnumeric():
+    # changing the data type of the variables (from str to int)
+    ticketType = int(ticketType)
+    seatNumber = int(seatNumber)
+
+    if ticketType == 2:
+        # making sure that the user hasn't entered an invalid seat number
+        if seatNumber <= 64:
+            # calculating the remainder
+            remainder = seatNumber % 4
+
+            if remainder == 0 or remainder == 1:
+                print(f"Your seat number is {seatNumber} , you are beside a window")
+            else:
+                print(f"Your seat number is {seatNumber} , you are not beside a window")
+        else:
+            print('Enter a valid seat number (at max 64)')
+
+    elif ticketType == 1:  # else if
+        # making sure that the user hasn't entered an invalid seat number
+        if seatNumber <= 47:
+            # calculating the remainder
+            remainder = seatNumber % 3
+
+            if remainder == 0 or remainder == 1 or seatNumber == 47:
+                print(f"Your seat number is {seatNumber} , you are beside a window")
+            else:
+                print(f"Your seat number is {seatNumber} , you are not beside a window")
+
+        else:
+            print('Enter a valid seat number (at max 47)')
+
+    else:
+        print('please enter a valid ticket type (1/2) only')
+
+else:
+    print('Please enter valid values')
